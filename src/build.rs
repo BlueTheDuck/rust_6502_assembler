7e8e197ac96806a0f0e88abdfa00ef26b2afd86f
PeronTheDuck <pedroignacioperezvargas@gmail.com>
2018-11-08 00:26:05 -0300

    Finished build script

--- src/build.rs
+++ src/build.rs
@@ -8,31 +8,44 @@ use std::{
 
 fn main() -> Result<(), Box<Error>> {
     let out_dir = env::var("OUT_DIR")?;
-    let mut opcode_code_generator: String = String::from("");
+    let mut opcode_code_generator: String = get_code();
 
-    opcode_code_generator = get_code();
-    let mut opcodemanagermod_code = include_str!("opcode_manager.rs");
-    let mut opcodemanagermod_code: Vec<&str> = opcodemanagermod_code.lines().collect();
-    let mut opcodemanagermod_code_total: String = "".to_string();
+    let mut opmanager_code = include_str!("opcode_manager.rs");
+    let mut opmanager_code_lines: Vec<&str> = opmanager_code.lines().collect();
+    let mut opmanager_code_final: String = "".to_string();
 
     println!("Finding");
 
-    let found:bool = false;
     let mut i = 0;
-    for line in &opcodemanagermod_code {
-        i = i + 1;
-        opcodemanagermod_code_total += &line;
-        opcodemanagermod_code_total += "\n";
-        if line == &"    //#region Opcode-static-list" {
-            println!("Found!");
-            //opcodemanagermod_code_total += &opcode_code_generator.clone();
+    let mut line: &str = opmanager_code_lines[i];
+    let mut state = 0; //SEARCH SKIP END
+    while i < opmanager_code_lines.len() {
+        line = opmanager_code_lines[i];
+        println!("<{}>", opmanager_code_lines[i]);
+        if state == 0 {
+            opmanager_code_final += &line;
+            opmanager_code_final += "\n";
+            if line == "    //#region Opcode-static-list" {
+                println!("Found marker");
+                state = 1;
+                opmanager_code_final += &opcode_code_generator;
+                opmanager_code_final += "\n";
+                println!("Code: <<{}>>", opmanager_code_final);
+            }
         }
-        if found&&line!="];" {
-
+        if state == 1 {
+            if line == "    //#endregion" {
+                state = 2;
+            }
         }
+        if state == 2 {
+            opmanager_code_final += &line;
+            opmanager_code_final += "\n";
+        }
+        i += 1;
     }
 
-    std::fs::write("src/opcode_manager.rs", opcodemanagermod_code_total);
+    std::fs::write("src/opcode_manager.rs", opmanager_code_final);
 
     Ok(())
 }
@@ -50,14 +63,14 @@ fn get_code() -> String {
 
         let items: Vec<&str> = line.split(",").collect();
         let code_line = format!(
-            "    create_opcode!(\"{}\", \"{}\", 0x{});\n",
+            "        create_opcode!(\"{}\", \"{}\", 0x{});\n",
             items[0], items[1], items[2]
         );
         opcode_code_generator += &code_line.clone();
     }
 
     opcode_code_generator = format!(
-        "static opcode_list: [Opcode; {}] = [\n{}];",
+        "    static opcode_list: [Opcode; {}] = [\n{}    ];",
         opcodes_counter, opcode_code_generator
     );
 
