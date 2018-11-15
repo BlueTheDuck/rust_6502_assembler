9d7d75a4b8e682b6f271adec56ec31101c868f2a
PeronTheDuck <pedroignacioperezvargas@gmail.com>
2018-11-14 21:15:20 -0300

    Refactored to lib

---
+++ src/lib/assembler.rs
@@ -0,0 +1,117 @@
+use lib::opcode_manager::{get_hex, identify_operand, AddressingModes, Opcode};
+
+pub mod data_types {
+    pub struct Bytes {
+        pub quant: usize,
+        pub bytes: [u8; 4],
+    }
+    impl Default for Bytes {
+        fn default() -> Self {
+            Bytes {
+                quant: 0,
+                bytes: [0, 0, 0, 0],
+            }
+        }
+    }
+    impl std::convert::From<u8> for Bytes {
+        fn from(n: u8) -> Self {
+            println!("Converting {:#X} to x16", n);
+            let mut b: Bytes = Bytes::default();
+            for i in 0..2 {
+                let disp: u64 = i * 8;
+                let i: usize = i as usize;
+                b.bytes[i] = ((n as u64 & (0xFFu64 << disp)) >> disp) as u8;
+            }
+            b.quant = 1;
+            b
+        }
+    }
+    impl std::convert::From<u16> for Bytes {
+        fn from(n: u16) -> Self {
+            println!("Converting {:#X} to x16", n);
+            let mut b: Bytes = Bytes::default();
+            for i in 0..2 {
+                let disp: u64 = i * 8;
+                let i: usize = i as usize;
+                b.bytes[i] = ((n as u64 & (0xFFu64 << disp)) >> disp) as u8;
+            }
+            b.quant = 1;
+            b
+        }
+    }
+    impl std::convert::From<u32> for Bytes {
+        fn from(n: u32) -> Self {
+            println!("Converting {:#X} to x32", n);
+            let mut b: Bytes = Bytes::default();
+            for i in 0..4 {
+                let disp: u64 = i * 8;
+                let i: usize = i as usize;
+                b.bytes[i] = ((n as u64 & (0xFFu64 << disp)) >> disp) as u8;
+            }
+            b.quant = 1;
+            b
+        }
+    }
+    impl std::fmt::UpperHex for Bytes {
+        fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+            let mut s: String = String::from("");
+            for i in 0..self.quant {
+                s = s + &format!("{:X}", self.bytes[i]);
+            }
+            write!(f, "{}", s)
+        }
+    }
+    impl std::fmt::Debug for Bytes {
+        fn fmt(&self,f: &mut std::fmt::Formatter) -> std::fmt::Result {
+            let mut out:String = "".to_string();
+            for i in 0..self.quant {
+                out = out+&format!("{:02}",format!("{:X}",self.bytes[i]));
+            }
+            println!("<{}>", out);
+            write!(f,"{}",out)
+        }
+    }
+}
+fn operand_to_bytes(operand: &str) -> data_types::Bytes {
+    lazy_static! {
+        static ref regex_extract_numbers: regex::Regex =
+            regex::Regex::new(r"\#?\$?(?P<d>[0-9A-Fa-f]+)").unwrap();
+    }
+
+    let clean_operand = regex_extract_numbers.replace_all(operand, "$d");
+    let mut numberic_operand: u32 =
+        u32::from_str_radix(&clean_operand, 16).expect("Couldn't parse operand");
+    if clean_operand.len() == 4 {
+        let temp = numberic_operand & 0xFF;
+        numberic_operand = (numberic_operand & 0xFF00) >> 8;
+        numberic_operand += temp << 8;
+        return data_types::Bytes::from(numberic_operand as u16);
+    } else if clean_operand.len() == 2 {
+        return data_types::Bytes::from(numberic_operand as u8);
+    }
+    assert!(false);
+    data_types::Bytes::default()
+}
+pub fn assemble_line(line: &String) -> Result<(&Opcode,data_types::Bytes),&str> {
+    let mut op_iter = line.split(" ");
+    let name = op_iter.next().unwrap();
+    let operand: Option<&str> = op_iter.next();
+    let binary_operand: data_types::Bytes;
+    let opcode: Option<&Opcode>;
+    let op_mode: AddressingModes;
+
+    if let Some(operand) = operand {
+        op_mode = identify_operand(operand);
+        binary_operand = operand_to_bytes(operand);
+    } else {
+        op_mode = AddressingModes::Implicit;
+        binary_operand = data_types::Bytes::default();
+    }
+    opcode = get_hex(name, op_mode);
+
+    if let Some(opcode) = opcode {
+        return Ok((opcode,binary_operand));
+    } else {
+        return Err("Couldn't assemble line");
+    }
+}
---
+++ src/lib/opcode_manager.rs
@@ -0,0 +1,126 @@
+use regex::Regex;
+
+//pub mod manager {
+mod operand_regexs {
+    lazy_static! {
+        pub static ref immediate: regex::Regex =
+            regex::Regex::new(r"\#\$[0-9A-F]{2}").expect("Failed parsing regex");
+        pub static ref absolute: regex::Regex =
+            regex::Regex::new(r"\$[0-9A-F]{4}").expect("Failed parsing regex");
+        pub static ref zero_page: regex::Regex =
+            regex::Regex::new(r"\$[0-9A-F]{2}").expect("Failed parsing regex");
+    }
+}
+
+//#region Datatypes
+#[derive(PartialEq, Debug, Clone, Copy)]
+pub enum AddressingModes {
+    Immediate, /* # */
+    Implicit,  /* impl */
+    Absolute,
+    ZeroPage,
+    Accumulator,
+    None,
+    Error,
+}
+#[derive(Debug)]
+pub struct Opcode<'a> {
+    pub name: &'a str,
+    pub addr_mode: AddressingModes,
+    pub value: u8,
+}
+//#endregion
+//#region Macros
+macro_rules! create_opcode {
+    ($name:expr,$addr_mode:expr,$val:expr) => {
+        Opcode {
+            name: $name,
+            addr_mode: $addr_mode,
+            value: $val,
+        }
+    };
+}
+//#endregion
+//#region Opcode-static-list
+static OPCODE_LIST: [Opcode; 53] = [
+    create_opcode!("ADC", AddressingModes::Immediate, 0x69),
+    create_opcode!("ADC", AddressingModes::Absolute, 0x6D),
+    create_opcode!("ADC", AddressingModes::ZeroPage, 0x65),
+    create_opcode!("AND", AddressingModes::Immediate, 0x29),
+    create_opcode!("AND", AddressingModes::Absolute, 0x2D),
+    create_opcode!("AND", AddressingModes::ZeroPage, 0x25),
+    create_opcode!("BIT", AddressingModes::Absolute, 0x2C),
+    create_opcode!("BIT", AddressingModes::ZeroPage, 0x24),
+    create_opcode!("CLV", AddressingModes::Implicit, 0xB8),
+    create_opcode!("CMP", AddressingModes::Immediate, 0xC9),
+    create_opcode!("CMP", AddressingModes::Absolute, 0xCD),
+    create_opcode!("CMP", AddressingModes::ZeroPage, 0xC5),
+    create_opcode!("CPX", AddressingModes::Immediate, 0xE0),
+    create_opcode!("CPX", AddressingModes::Absolute, 0xEC),
+    create_opcode!("CPX", AddressingModes::ZeroPage, 0xE4),
+    create_opcode!("CPY", AddressingModes::Immediate, 0xC0),
+    create_opcode!("CPY", AddressingModes::Absolute, 0xCC),
+    create_opcode!("CPY", AddressingModes::ZeroPage, 0xC4),
+    create_opcode!("DEC", AddressingModes::Absolute, 0xCE),
+    create_opcode!("DEC", AddressingModes::ZeroPage, 0xC6),
+    create_opcode!("DEX", AddressingModes::Implicit, 0xCA),
+    create_opcode!("DEY", AddressingModes::Implicit, 0x88),
+    create_opcode!("INC", AddressingModes::Absolute, 0xEE),
+    create_opcode!("INC", AddressingModes::ZeroPage, 0xE6),
+    create_opcode!("INX", AddressingModes::Implicit, 0xE8),
+    create_opcode!("INY", AddressingModes::Implicit, 0xC8),
+    create_opcode!("JMP", AddressingModes::Absolute, 0x4C),
+    create_opcode!("LDA", AddressingModes::Immediate, 0xA9),
+    create_opcode!("LDA", AddressingModes::Absolute, 0xAD),
+    create_opcode!("LDA", AddressingModes::ZeroPage, 0xA5),
+    create_opcode!("LDX", AddressingModes::Immediate, 0xA2),
+    create_opcode!("LDX", AddressingModes::Absolute, 0xAE),
+    create_opcode!("LDX", AddressingModes::ZeroPage, 0xA6),
+    create_opcode!("LDY", AddressingModes::Immediate, 0xA0),
+    create_opcode!("LDY", AddressingModes::Absolute, 0xAC),
+    create_opcode!("LDY", AddressingModes::ZeroPage, 0xA4),
+    create_opcode!("LSR", AddressingModes::Absolute, 0x4E),
+    create_opcode!("LSR", AddressingModes::ZeroPage, 0x46),
+    create_opcode!("LSR", AddressingModes::Implicit, 0x4A),
+    create_opcode!("NOP", AddressingModes::Implicit, 0xEA),
+    create_opcode!("PHA", AddressingModes::Implicit, 0x48),
+    create_opcode!("PLA", AddressingModes::Implicit, 0x68),
+    create_opcode!("RTS", AddressingModes::Implicit, 0x60),
+    create_opcode!("STA", AddressingModes::Absolute, 0x8D),
+    create_opcode!("STA", AddressingModes::ZeroPage, 0x85),
+    create_opcode!("STX", AddressingModes::Absolute, 0x8E),
+    create_opcode!("STX", AddressingModes::ZeroPage, 0x86),
+    create_opcode!("STY", AddressingModes::Absolute, 0x8C),
+    create_opcode!("STY", AddressingModes::ZeroPage, 0x84),
+    create_opcode!("TAX", AddressingModes::Implicit, 0xAA),
+    create_opcode!("TAY", AddressingModes::Implicit, 0xA8),
+    create_opcode!("TXA", AddressingModes::Implicit, 0x8A),
+    create_opcode!("TYA", AddressingModes::Implicit, 0x98),
+];
+//#endregion
+
+pub fn identify_operand(operand: &str) -> AddressingModes {
+    if operand_regexs::immediate.is_match(operand) {
+        return AddressingModes::Immediate;
+    }
+    if operand_regexs::absolute.is_match(operand) {
+        return AddressingModes::Absolute;
+    }
+    if operand_regexs::zero_page.is_match(operand) {
+        return AddressingModes::ZeroPage;
+    }
+
+    AddressingModes::Error
+}
+pub fn get_hex(opcode: &str, operand_mode: AddressingModes) -> Option<&Opcode> {
+    let mut i = 0;
+    while i < OPCODE_LIST.len() {
+        let actual_op: &Opcode = &OPCODE_LIST[i];
+        if actual_op.name == opcode && actual_op.addr_mode == operand_mode {
+            return Some(&OPCODE_LIST[i]);
+        }
+        i += 1;
+    }
+    None
+}
+//}
