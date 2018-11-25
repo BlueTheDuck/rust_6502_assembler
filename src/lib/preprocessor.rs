e2a2c980c796748a18788847f4da729aa71aded9
PeronTheDuck <pedroignacioperezvargas@gmail.com>
2018-11-25 19:13:45 -0300

    Started rework on directives preprocessor's operand parser

--- src/lib/preprocessor.rs
+++ src/lib/preprocessor.rs
@@ -12,21 +12,28 @@ pub enum ParamTypes {
     None,
 }
 impl std::convert::From<String> for ParamTypes {
-    fn from(f: String) -> ParamTypes {
-        let f = f.trim();
+    fn from(from: String) -> ParamTypes {
+        let f = from.trim();
+        lazy_static! {
+            //static ref regex_extract_numbers: regex::Regex = regex::Regex::new(r"\#?\$?(?P<d>[0-9A-Fa-f]+)").unwrap();
+            static ref rgx_ubyte
+
+        }
+        /*let clean_operand = regex_extract_numbers.replace_all(&from, "$d").to_string();
+        let numberic_operand = u32::from_str_radix(&clean_operand, 16).expect("Couldn't parse operand"); */
+
         //#$xx -> 4
         //#$xxyy -> 6
         //$xxyy -> 5
+
         match f.len() {
-            4 => {
-                ParamTypes::UByte(u8::from_str_radix(&f[2..4], 16).expect("Couldn't parse to u8"))
+            4 => ParamTypes::UByte(u8::from_str_radix(&f[2..4], 16).expect("Couldn't parse to u8")),
+            5 => {
+                ParamTypes::Addr(u16::from_str_radix(&f[1..5], 16).expect("Couldn't parse to u16"))
             }
-            5 => ParamTypes::Addr(
-                u16::from_str_radix(&f[1..5], 16).expect("Couldn't parse to u16"),
+            6 => ParamTypes::UBhort(
+                u16::from_str_radix(&f[2..6], 16).expect("Couldn't parse to u16"),
             ),
-            6 => {
-                ParamTypes::UBhort(u16::from_str_radix(&f[2..6], 16).expect("Couldn't parse to u16"))
-            }
             _ => ParamTypes::None,
         }
     }
@@ -41,7 +48,7 @@ pub static DIRECTIVES: [(DirectiveName, DirectiveFn); 1] =
     [include_directive!(".org", "./directives/org.rs")];
 
 pub fn find_directive(name: &str) -> Option<usize> {
-    let mut i:usize = 0;
+    let mut i: usize = 0;
     for directive in DIRECTIVES.iter() {
         if directive.0 == name {
             return Some(i);
