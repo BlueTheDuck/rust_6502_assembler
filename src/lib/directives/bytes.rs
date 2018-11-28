d834f3474b2bf10eb076da79f18955cbbae043bf
PeronTheDuck <pedroignacioperezvargas@gmail.com>
2018-11-27 23:09:50 -0300

    Added .bytes

--- src/lib/directives/bytes.rs
+++ src/lib/directives/bytes.rs
@@ -1,3 +1,11 @@
 |state,param|{
-    println!("{:#?}",param);
+    let bs;
+    match param {
+        ParamTypes::MultiBytes(e) => bs = e,
+        _=>panic!(".bytes takes a comma separated list of bytes")
+    }
+    println!("{:#?}",bs);
+    for b in bs {
+        state.push_byte(b);
+    }
 }
